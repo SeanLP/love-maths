e76065679741bb59ce2eac7f10db277d69dbf25a
SeanLP <seanpreston9213@gmail.com>
2021-10-10 20:18:19 +0000

    Add code to division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,10 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    }else if(gameType === "subtract"){
+    }else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    }else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     }else {
         alert(`Unkown game type: ${gameType}`);
         throw `Unkown game type: ${gameType}. Aborting!`;
@@ -89,10 +91,12 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return[operand1 + operand2, "addition"];
-    } else if(operator === "x") {
+    } else if (operator === "x") {
         return[operand1 * operand2, "multiply"];
-    }else if(operator === "-"){
+    }else if (operator === "-") {
         return[operand1 - operand2, "subtract"];
+    }else if (operator === "/") {
+        return[operand1 / operand2, "division"]
     }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -138,6 +142,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "x";
 }
 
-function displayDivideQuestion() {
-    
+function displayDivisionQuestion(operand1, operand2) {
+
+    operand1 = operand1 * operand2;
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
